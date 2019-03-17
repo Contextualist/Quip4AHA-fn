2e2d86c2200f900536f777906eccf6972cfb77ad
Contextualist <harzjc@gmail.com>
2019-03-16 22:41:24 -0400

    New forecast providers to MetaWeather & Weatherbit.io
    Wunderground no longer provides free service.

--- UpdateWeather.py
+++ UpdateWeather.py
@@ -1,30 +1,27 @@
 from quip4aha import q4a, week, InvalidOperation
 import re
-import json
-from urllib.request import urlopen # py2, urllib2
+import requests
+from datetime import datetime, timedelta
 from collections import namedtuple
 
 SIMP = {'Clear':'sunny',
-        'Mostly Cloudy':'mostly cloudy', 'Partly Cloudy':'partly cloudy', 'Overcast':'cloudy',
-        'Thunderstorm':'rainy', 'Chance of a Thunderstorm':'rainy', 'Rain':'rainy', 'Chance of Rain':'rainy'}
+        'Heavy Cloud':'mostly cloudy', 'Light Cloud':'partly cloudy',
+        'Thunderstorm':'rainy', 'Heavy Rain':'rainy', 'Light Rain':'rainy', 'Showers':'rainy'}
 
 def precondition():
-    dayn = week.DaysTo('next Wednesday')
-    if dayn > 3:
-        raise InvalidOperation("Unable to get the weather for Wednesday: "
-                               "WunderStation only gives prediction for today and 3 days ahead. \n"
-                              f"But it's {dayn} days to next Wednesday.")
-    return dayn
+    return week.DaysTo('next Wednesday')
 
 def fetch_weather(next_nday):
-    response = json.loads(urlopen(
-        "http://api.wunderground.com/api/01702baefa3fbf8e/forecast/q/zmw:00000.1.59287.json").read())
-    print(response)
-    data = response['forecast']['simpleforecast']['forecastday'][next_nday]
+    r_wb = requests.get("https://api.weatherbit.io/v2.0/forecast/daily",
+           params={'city': 'Guangzhou', 'days': next_nday+1, 'key': '8a5c6384bc4e45d5bba4fb0015d96c6f'}) \
+           .json()['data'][-1] # Weatherbit: for rain_percen only
+    wnd = (datetime.today().date() + timedelta(next_nday)).strftime("%Y/%m/%d")
+    r = requests.get(f"https://www.metaweather.com/api/location/2161838/{wnd}/") \
+        .json()[0]
     return namedtuple('weather', 'condition rain_percen tempC tempF')(
-        condition=SIMP.get(data['conditions'], data['conditions'].lower()),
-        rain_percen=data['pop'],
-        tempC=data['high']['celsius'], tempF=data['high']['fahrenheit'],
+        condition=SIMP.get(r['weather_state_name'], r['weather_state_name'].lower()),
+        rain_percen=r_wb['pop'],
+        tempC=round(r['max_temp']), tempF=round(r['max_temp']*9/5+32),
     )
 
 def compose(doc_id, w):
@@ -36,7 +33,7 @@ def compose(doc_id, w):
         sid,
         f"<p class='line'>{host}Good Morning AHA!<br/>"
         f"It is Wednesday, {date}. The weather for today is {w.condition}. "
-        f"There is {'an' if w.rain_percen==80 else 'a'} {w.rain_percen}% chance of rain. "
+        f"There is {'an' if w.rain_percen in [18,*range(80,90)] else 'a'} {w.rain_percen}% chance of rain. "
         f"The high temperature today will be {w.tempC} degrees Celsius, which is {w.tempF} degrees Fahrenheit.</p>"
     )
 
@@ -49,8 +46,7 @@ def UpdateWeather():
     weather = fetch_weather(next_nday)
     doc_id = q4a.latest_script_id
     sid, p = compose(doc_id, weather)
-    print(next_nday, weather, doc_id, sid, p)
-    #post_weather(doc_id, sid, p)
+    post_weather(doc_id, sid, p)
     return "Done!"
 
 if __name__=='__main__':
